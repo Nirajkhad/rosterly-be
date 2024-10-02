4e2498f7f247726d7883cfca80cf716abf3c7836
suraj rai <surajrai1930@gmail.com>
2024-10-02 22:23:23 +0545

    fixes: migrate

--- src/migrations/20240930161246-create-user.js
+++ src/migrations/20240930161246-create-user.js
@@ -1,54 +1,50 @@
-'use strict';
-const { Model, DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
-    }
-  }
-
-  User.init({
-    uuid: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    user_type: {
-      type: DataTypes.ENUM('admin', 'user'),
-      allowNull: false,
-    },
-    phone_number: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password_hash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    deleted_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users',
-    timestamps: true,
-    createdAt: 'created_at', // Use snake_case for the DB column name
-    updatedAt: 'updated_at', // Use snake_case for the DB column name
-    paranoid: true, // Enables soft deletes
-  });
-
-  return User;
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      full_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      user_type: {
+        type: Sequelize.STRING,
+      },
+      phone_number: {
+        type: Sequelize.STRING,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('users');
+  },
 };
